57789d68daaf92a6161c0b2bf5c8d9e6374e6b84
Kathryn Leavitt <138052070+kittycore@users.noreply.github.com>
2023-07-10 12:34:58 -0500

    Add wildcard

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,17 @@
+import glob, os
+
+from typing import Iterator
+
+
+def wildcard(directory: str, extension: str) -> Iterator[str]:
+    files = glob.glob(os.path.join(directory, f'*.{extension}'))
+    for path in files:
+        yield path
+
+
 def main() -> None:
-    print('Hello, world!')
+    for path in wildcard('.', '*'):
+        print(path)
 
 
 if __name__ == '__main__':
