129b53f4980dccb53eec4ef401cae70f206a7675
Kathryn Leavitt <138052070+kittycore@users.noreply.github.com>
2023-07-10 12:36:12 -0500

    Add docstring to wildcard

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,17 @@ from typing import Iterator
 
 
 def wildcard(directory: str, extension: str) -> Iterator[str]:
+    '''Searches for files ending with the given `extension` within
+    `directory` and yields the file path of each matching file.
+
+    Args:
+        directory (str): The directory to search within for files.
+        extension (str): The file extension to limit the search to.
+
+    Yields:
+        str: The file path of each matching file.
+    '''
+
     files = glob.glob(os.path.join(directory, f'*.{extension}'))
     for path in files:
         yield path
